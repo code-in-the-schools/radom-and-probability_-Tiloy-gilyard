f831ce4226745424d051b37397a4a53b37c0723d
Tiloy84 <tiloy67@gmail.com>
2020-07-20 18:31:59 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import randdom
+randomlist1- 10
+for e in range(10):
+r= random.randint(1,6)
+randomlist.append(r)
+for e in ransomelist1
+print(randomlist[e])
+# of 1terations`: 10
+#1 = 0%
+#2 = 10%
+#3 = 0%
+#4 = 10%
+#4 = 10%
+#5 = 10%
+#6 = 10%
+#7 = 10%
+#8 = 40%
+#10= 0%
+
+
+
+randomlist2-[10]
+for g in range (1,6)
+d = random.randint(1,100)
+randlist2.append(d)
+for g in randlist2:
+  print(randlist2{g})
